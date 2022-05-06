fe24b9f7848761ae803e4240b95afde073245d14
ghector6 <ghectormus@gmail.com>
2022-05-06 13:10:55 -0500

    Modificando Reader.test.js: Eliminando elemento no deseado

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,7 +1,5 @@
 const Reader = require("./../../lib/utils/Reader")
 
-const { TestScheduler } = require("jest")
-
 describe("Test to see if my Reader is working", () =>{
     test("if the reader gets elements in the json", () => {
         const students = Reader.readJsonFile("./students.json")
