44331981b333a57d4c40ec852abdcb7c922a7d29
ghector6 <ghectormus@gmail.com>
2022-05-06 19:06:37 -0500

    Subiendo prueba modificada para que funcione y corroborar los actions que funcionen tambie

--- test/services/studentService.test.js
+++ test/services/studentService.test.js
@@ -6,6 +6,6 @@ describe("Test to se if static method gets all elementes from the json" , () => 
     test("if my method prints the whole students list with all elements", () => {
         const students = Reader.readJsonFile("./students.json")
         const wholeList = studentService.printElements(students)
-        expect(wholeList.length).toBe(50)
+        expect(wholeList.length).toBe(51)
     })
 })
