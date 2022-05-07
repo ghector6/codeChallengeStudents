66b1c4a5e9b77387317178c2ac7f16796c90dc38
ghector6 <ghectormus@gmail.com>
2022-05-06 19:04:41 -0500

    Subiendo prueba que truena para revisar que actions esté funcionando

---
+++ test/services/studentService.test.js
@@ -0,0 +1,11 @@
+const Reader = require("./../../lib/utils/Reader")
+const studentService = require("./../../lib/services/studentService")
+
+describe("Test to se if static method gets all elementes from the json" , () => {
+    
+    test("if my method prints the whole students list with all elements", () => {
+        const students = Reader.readJsonFile("./students.json")
+        const wholeList = studentService.printElements(students)
+        expect(wholeList.length).toBe(50)
+    })
+})
