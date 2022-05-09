7e33d62066de99853ab33fc8de24f70bbddadc78
ghector6 <ghectormus@gmail.com>
2022-05-09 12:45:02 -0500

    Agregar prueba para respaldar metodo nuevo de credit service

---
+++ test/services/creditService.test.js
@@ -0,0 +1,11 @@
+const Reader = require("./../../lib/utils/Reader")
+const creditService = require("./../../lib/services/creditService")
+
+describe("Test to see if Im getting students credits", () =>{
+    test("if it is getting < 500 credits students", () => {
+        const students = Reader.readJsonFile("./students.json")
+        const studentsWithValidation = creditService.getStudentsWithCredits(students,)
+        expect(studentsWithValidation.length).toBe(27)
+    })
+
+})
