366f1dc0fc5f851279cee61a6df38fc48d8048d7
ghector6 <ghectormus@gmail.com>
2022-05-09 13:10:46 -0500

    Modificando syntax de todo con lint

--- test/services/studentService.test.js
+++ test/services/studentService.test.js
@@ -1,17 +1,17 @@
-const Reader = require("./../../lib/utils/Reader")
-const studentService = require("./../../lib/services/studentService")
+const Reader = require("./../../lib/utils/Reader");
+const studentService = require("./../../lib/services/studentService");
 
 describe("Test to se if static method gets all elementes from the json" , () => {
     
     test("if my method prints the whole students list with all elements", () => {
-        const students = Reader.readJsonFile("./students.json")
-        const wholeList = studentService.printElements(students)
-        expect(wholeList.length).toBe(51)
-    })
+        const students = Reader.readJsonFile("./students.json");
+        const wholeList = studentService.printElements(students);
+        expect(wholeList.length).toBe(51);
+    });
 
     test("2) if im getting the list of students with certification", () => {
-        const students = Reader.readJsonFile("./students.json")
-        const studentsValidated = studentService.studentsWithCertification(students, true)
-        expect(studentsValidated[1]).toBe("Lucinda")
-    })
-})
+        const students = Reader.readJsonFile("./students.json");
+        const studentsValidated = studentService.studentsWithCertification(students, true);
+        expect(studentsValidated[1]).toBe("Lucinda");
+    });
+});
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,9 +1,9 @@
-const Reader = require("./../../lib/utils/Reader")
+const Reader = require("./../../lib/utils/Reader");
 
 describe("Test to see if my Reader is working", () =>{
     test("if the reader gets elements in the json", () => {
-        const students = Reader.readJsonFile("./students.json")
-        expect(students.length).toBe(51)
-    })
-})
+        const students = Reader.readJsonFile("./students.json");
+        expect(students.length).toBe(51);
+    });
+});
 
