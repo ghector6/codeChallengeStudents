d954cc5c4059863570a1b4c674d33803f420d9c3
ghector6 <ghectormus@gmail.com>
2022-05-11 16:41:18 -0500

    Terminando codeChallenge crriendo linter

--- test/controllers/studentController.test.js
+++ test/controllers/studentController.test.js
@@ -1,20 +1,20 @@
-const studentService = require("../../lib/services/studentService")
-const studentController = require("./../../lib/controllers/studentController")
+const studentService = require("../../lib/services/studentService");
+const studentController = require("./../../lib/controllers/studentController");
 
 describe("Tests for the controller", () => {
     
     test("To see if im getting the whole list", () => {
-        const students = studentController.getWholeListStudents()
-        expect(students.length).toBe(51)
-    })
+        const students = studentController.getWholeListStudents();
+        expect(students.length).toBe(51);
+    });
 
     test("to see if im getting the true certs", () => {
-        const students = studentController.getStudsWithCert(true)
-        expect(students.length).toBe(29)
-    })
+        const students = studentController.getStudsWithCert(true);
+        expect(students.length).toBe(29);
+    });
 
     test ("to check for credits in students", () => {
-        const students = studentController.getStudsCreds(500)
-        expect(students.length).toBe(27)
-    })
-})
+        const students = studentController.getStudsCreds(500);
+        expect(students.length).toBe(27);
+    });
+});
